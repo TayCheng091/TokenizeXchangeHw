72f94363e40fe096e7bfa1d53a0494c323daffa5
Tay <b1233967@gmail.com>
2025-01-10 13:15:49 +0800

    feat: overflow scroll for card list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,12 @@ import {
   Subscription,
   debounceTime,
   distinctUntilChanged,
+  tap,
 } from 'rxjs';
 
 import { CryptoPrice } from '../../models/share.model';
 import { CryptoService } from '../../services/crypto.service';
+import { LoadingService } from '../../services/loading.service';
 
 @Component({
   selector: 'app-home',
@@ -17,22 +19,44 @@ export class HomeComponent implements OnInit, OnDestroy {
   cryptoPrices: CryptoPrice[] = [];
   filteredPrices: CryptoPrice[] = [];
   searchTerm = '';
+  error = '';
+  isSearching = false;
   private searchSubject = new Subject<string>();
   private searchSubscription?: Subscription;
   private debounceTime = 500;
 
-  constructor(private cryptoService: CryptoService) {
+  constructor(
+    private cryptoService: CryptoService,
+    private loadingService: LoadingService
+  ) {
     this.searchSubscription = this.searchSubject
-      .pipe(debounceTime(this.debounceTime), distinctUntilChanged())
-      .subscribe((term) => {
-        this.filterCryptos(term);
+      .pipe(
+        tap(() => this.loadingService.show()),
+        debounceTime(this.debounceTime),
+        distinctUntilChanged()
+      )
+      .subscribe({
+        next: (term) => {
+          this.filterCryptos(term);
+          this.loadingService.hide();
+        },
+        error: () => {
+          this.loadingService.hide();
+        },
       });
   }
 
   ngOnInit() {
-    this.cryptoService.getPrices().subscribe((prices) => {
-      this.cryptoPrices = prices;
-      this.filteredPrices = prices;
+    this.cryptoService.getPrices().subscribe({
+      next: (prices) => {
+        this.cryptoPrices = prices;
+        this.filteredPrices = prices;
+        this.error = '';
+      },
+      error: (error) => {
+        console.error('Failed to fetch prices:', error);
+        this.error = 'Failed to load crypto prices. Please try again later.';
+      },
     });
   }
 
